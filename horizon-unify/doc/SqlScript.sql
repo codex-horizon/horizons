0d3682fc8ab5df4c815fd85f07749cd0bc0375b7
吴炳成 <bingcheng.wu@newtouch.com>
2023-10-11 16:22:31 +0800

    oauth2表结构ddl语句

--- horizon-unify/doc/SqlScript.sql
+++ horizon-unify/doc/SqlScript.sql
@@ -1,2 +1,60 @@
--- 官方oauth2表结构：https://github.com/spring-attic/spring-security-oauth/blob/main/spring-security-oauth2/src/test/resources/schema.sql
--- oauth2表字段说明：https://blog.csdn.net/wangxuelei036/article/details/109491215
+CREATE TABLE `oauth_access_token`
+(
+    `token_id`          varchar(256) DEFAULT NULL COMMENT 'MD5加密的access_token的值',
+    `token`             blob COMMENT 'OAuth2AccessToken.java对象序列化后的二进制数据',
+    `authentication_id` varchar(256) NOT NULL COMMENT 'MD5加密过的username,client_id,scope',
+    `user_name`         varchar(256) DEFAULT NULL COMMENT '登录的用户名',
+    `client_id`         varchar(256) DEFAULT NULL COMMENT '客户端ID',
+    `authentication`    blob COMMENT 'OAuth2Authentication.java对象序列化后的二进制数据',
+    `refresh_token`     varchar(256) DEFAULT NULL COMMENT 'MD5加密后的refresh_token的值',
+    PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='访问令牌';
+
+CREATE TABLE `oauth_approvals`
+(
+    `userId`         varchar(256) DEFAULT NULL COMMENT '登录的用户名',
+    `clientId`       varchar(256) DEFAULT NULL COMMENT '客户端ID',
+    `scope`          varchar(256) DEFAULT NULL COMMENT '申请的权限',
+    `status`         varchar(10)  DEFAULT NULL COMMENT '状态(Approve或Deny)',
+    `expiresAt`      datetime     DEFAULT NULL COMMENT '过期时间',
+    `lastModifiedAt` datetime     DEFAULT NULL COMMENT '最终修改时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权记录';
+
+CREATE TABLE `oauth_client_details`
+(
+    `client_id`               varchar(256) NOT NULL COMMENT '客户端ID',
+    `resource_ids`            varchar(256)  DEFAULT NULL COMMENT '资源ID集合，多个资源时用英文逗号分隔',
+    `client_secret`           varchar(256)  DEFAULT NULL COMMENT '客户端密匙',
+    `scope`                   varchar(256)  DEFAULT NULL COMMENT '客户端申请的权限范围',
+    `authorized_grant_types`  varchar(256)  DEFAULT NULL COMMENT '客户端支持的grant_type',
+    `web_server_redirect_uri` varchar(256)  DEFAULT NULL COMMENT '重定向URI',
+    `authorities`             varchar(256)  DEFAULT NULL COMMENT '客户端所拥有的SpringSecurity的权限值，多个用英文逗号分隔',
+    `access_token_validity`   int(11) DEFAULT NULL COMMENT '访问令牌有效时间值(单位秒)',
+    `refresh_token_validity`  int(11) DEFAULT NULL COMMENT '更新令牌有效时间值(单位秒)',
+    `additional_information`  varchar(4096) DEFAULT NULL COMMENT '预留字段',
+    `autoapprove`             varchar(256)  DEFAULT NULL COMMENT '用户是否自动Approval操作',
+    PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='客户端信息';
+
+CREATE TABLE `oauth_client_token`
+(
+    `token_id`          varchar(256) DEFAULT NULL COMMENT 'MD5加密的access_token值',
+    `token`             blob COMMENT 'OAuth2AccessToken.java对象序列化后的二进制数据',
+    `authentication_id` varchar(256) NOT NULL COMMENT 'MD5加密过的username,client_id,scope',
+    `user_name`         varchar(256) DEFAULT NULL COMMENT '登录的用户名',
+    `client_id`         varchar(256) DEFAULT NULL COMMENT '客户端ID',
+    PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='该表用于在客户端系统中存储从服务端获取的token数据';
+
+CREATE TABLE `oauth_code`
+(
+    `code`           varchar(256) DEFAULT NULL COMMENT '授权码(未加密)',
+    `authentication` blob COMMENT 'AuthorizationRequestHolder.java对象序列化后的二进制数据'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权码';
+
+CREATE TABLE `oauth_refresh_token`
+(
+    `token_id`       varchar(256) DEFAULT NULL COMMENT 'MD5加密过的refresh_token的值',
+    `token`          blob COMMENT 'OAuth2RefreshToken.java对象序列化后的二进制数据',
+    `authentication` blob COMMENT 'OAuth2Authentication.java对象序列化后的二进制数据'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='更新令牌';
